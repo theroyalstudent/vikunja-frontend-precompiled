6108fac18ee5e9a742690280c648dab3a058fb40
Edwin A <edwin@theroyalstudent.com>
2020-08-25 04:46:09 +0800

    Compiled commit 30f6e39 - Update vue monorepo to v2.6.12 (#215)
    Update vue monorepo to v2.6.12
    
    Reviewed-on: https://kolaente.dev/vikunja/frontend/pulls/215

---
+++ precache-manifest.3ec5b38fde30f551e285d03b4b2604a7.js
@@ -0,0 +1,482 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a67931e5df0a8d7c94e3",
+    "url": "/css/app.5342f97d.css"
+  },
+  {
+    "revision": "758459fa343dbfd43db7",
+    "url": "/css/chunk-vendors.02617090.css"
+  },
+  {
+    "revision": "f0f69663654ba985693f",
+    "url": "/css/editor.42cd64b7.css"
+  },
+  {
+    "revision": "adcaee5f63f9ebe304ca",
+    "url": "/css/settings.6655d893.css"
+  },
+  {
+    "revision": "5d12c2828e34801d2a41",
+    "url": "/css/user-settings.16cf3421.css"
+  },
+  {
+    "revision": "148a6749baa5f658a45183ddb5ee159f",
+    "url": "/fonts/open-sans-v15-latin-700.148a6749.eot"
+  },
+  {
+    "revision": "623e3205570002af47fc2b88f9335d19",
+    "url": "/fonts/open-sans-v15-latin-700.623e3205.woff"
+  },
+  {
+    "revision": "7e08cc656863d52bcb5cd34805ac605b",
+    "url": "/fonts/open-sans-v15-latin-700.7e08cc65.ttf"
+  },
+  {
+    "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
+    "url": "/fonts/open-sans-v15-latin-700.d08c09f2.woff2"
+  },
+  {
+    "revision": "148a6749baa5f658a45183ddb5ee159f",
+    "url": "/fonts/open-sans-v15-latin-700.eot"
+  },
+  {
+    "revision": "2e00b2635b51ba336b4b67a5d0bc03c7",
+    "url": "/fonts/open-sans-v15-latin-700.svg"
+  },
+  {
+    "revision": "7e08cc656863d52bcb5cd34805ac605b",
+    "url": "/fonts/open-sans-v15-latin-700.ttf"
+  },
+  {
+    "revision": "623e3205570002af47fc2b88f9335d19",
+    "url": "/fonts/open-sans-v15-latin-700.woff"
+  },
+  {
+    "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
+    "url": "/fonts/open-sans-v15-latin-700.woff2"
+  },
+  {
+    "revision": "437e5048081ea2d5cd1575ff06c78148",
+    "url": "/fonts/open-sans-v15-latin-700italic.437e5048.eot"
+  },
+  {
+    "revision": "72e19cbb0e38c6773a7751156752cec4",
+    "url": "/fonts/open-sans-v15-latin-700italic.72e19cbb.woff"
+  },
+  {
+    "revision": "c02f5da6e82e1efe0b45841bfd49ce37",
+    "url": "/fonts/open-sans-v15-latin-700italic.c02f5da6.woff2"
+  },
+  {
+    "revision": "cffddf70e3f007ddacfca725283f8e28",
+    "url": "/fonts/open-sans-v15-latin-700italic.cffddf70.ttf"
+  },
+  {
+    "revision": "437e5048081ea2d5cd1575ff06c78148",
+    "url": "/fonts/open-sans-v15-latin-700italic.eot"
+  },
+  {
+    "revision": "f445cf25b8e059587318c7ee9014154b",
+    "url": "/fonts/open-sans-v15-latin-700italic.svg"
+  },
+  {
+    "revision": "cffddf70e3f007ddacfca725283f8e28",
+    "url": "/fonts/open-sans-v15-latin-700italic.ttf"
+  },
+  {
+    "revision": "72e19cbb0e38c6773a7751156752cec4",
+    "url": "/fonts/open-sans-v15-latin-700italic.woff"
+  },
+  {
+    "revision": "c02f5da6e82e1efe0b45841bfd49ce37",
+    "url": "/fonts/open-sans-v15-latin-700italic.woff2"
+  },
+  {
+    "revision": "987032ea5d57c93d8da215678eae3b86",
+    "url": "/fonts/open-sans-v15-latin-italic.987032ea.woff2"
+  },
+  {
+    "revision": "9b695c7dff832fca56d270d8fd90cd3c",
+    "url": "/fonts/open-sans-v15-latin-italic.9b695c7d.eot"
+  },
+  {
+    "revision": "db70d0b9cb27ada1a260a2b35e756b8b",
+    "url": "/fonts/open-sans-v15-latin-italic.db70d0b9.woff"
+  },
+  {
+    "revision": "9b695c7dff832fca56d270d8fd90cd3c",
+    "url": "/fonts/open-sans-v15-latin-italic.eot"
+  },
+  {
+    "revision": "f42df73dcb75559a62c2e654035a784e",
+    "url": "/fonts/open-sans-v15-latin-italic.f42df73d.ttf"
+  },
+  {
+    "revision": "fd2ba7503f70aafcdcc45211a481684d",
+    "url": "/fonts/open-sans-v15-latin-italic.svg"
+  },
+  {
+    "revision": "f42df73dcb75559a62c2e654035a784e",
+    "url": "/fonts/open-sans-v15-latin-italic.ttf"
+  },
+  {
+    "revision": "db70d0b9cb27ada1a260a2b35e756b8b",
+    "url": "/fonts/open-sans-v15-latin-italic.woff"
+  },
+  {
+    "revision": "987032ea5d57c93d8da215678eae3b86",
+    "url": "/fonts/open-sans-v15-latin-italic.woff2"
+  },
+  {
+    "revision": "9dce7f01715340861bdb57318e2f3fdc",
+    "url": "/fonts/open-sans-v15-latin-regular.9dce7f01.eot"
+  },
+  {
+    "revision": "bf2d0783515b7d75c35bde69e01b3135",
+    "url": "/fonts/open-sans-v15-latin-regular.bf2d0783.woff"
+  },
+  {
+    "revision": "c045b73d86803686f4cd1cc3f9ceba59",
+    "url": "/fonts/open-sans-v15-latin-regular.c045b73d.ttf"
+  },
+  {
+    "revision": "cffb686d7d2f4682df8342bd4d276e09",
+    "url": "/fonts/open-sans-v15-latin-regular.cffb686d.woff2"
+  },
+  {
+    "revision": "9dce7f01715340861bdb57318e2f3fdc",
+    "url": "/fonts/open-sans-v15-latin-regular.eot"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/fonts/open-sans-v15-latin-regular.svg"
+  },
+  {
+    "revision": "c045b73d86803686f4cd1cc3f9ceba59",
+    "url": "/fonts/open-sans-v15-latin-regular.ttf"
+  },
+  {
+    "revision": "bf2d0783515b7d75c35bde69e01b3135",
+    "url": "/fonts/open-sans-v15-latin-regular.woff"
+  },
+  {
+    "revision": "cffb686d7d2f4682df8342bd4d276e09",
+    "url": "/fonts/open-sans-v15-latin-regular.woff2"
+  },
+  {
+    "revision": "31faf209dd0cf535d911bdd9863e5c01",
+    "url": "/fonts/quicksand-v7-latin-300.31faf209.woff"
+  },
+  {
+    "revision": "3704330fe76fa5e3dffef582b976e4cf",
+    "url": "/fonts/quicksand-v7-latin-300.3704330f.eot"
+  },
+  {
+    "revision": "64c76c78f8892c6827204bb2887cf051",
+    "url": "/fonts/quicksand-v7-latin-300.64c76c78.ttf"
+  },
+  {
+    "revision": "e899112139f3733efea922ec3a4276c6",
+    "url": "/fonts/quicksand-v7-latin-300.e8991121.woff2"
+  },
+  {
+    "revision": "3704330fe76fa5e3dffef582b976e4cf",
+    "url": "/fonts/quicksand-v7-latin-300.eot"
+  },
+  {
+    "revision": "4e010b929d0b71314a368114b9c66a83",
+    "url": "/fonts/quicksand-v7-latin-300.svg"
+  },
+  {
+    "revision": "64c76c78f8892c6827204bb2887cf051",
+    "url": "/fonts/quicksand-v7-latin-300.ttf"
+  },
+  {
+    "revision": "31faf209dd0cf535d911bdd9863e5c01",
+    "url": "/fonts/quicksand-v7-latin-300.woff"
+  },
+  {
+    "revision": "e899112139f3733efea922ec3a4276c6",
+    "url": "/fonts/quicksand-v7-latin-300.woff2"
+  },
+  {
+    "revision": "365396fdb9907773718c78332af9d536",
+    "url": "/fonts/quicksand-v7-latin-500.365396fd.eot"
+  },
+  {
+    "revision": "53b5698c04e815736c8d5f91984b8945",
+    "url": "/fonts/quicksand-v7-latin-500.53b5698c.ttf"
+  },
+  {
+    "revision": "54a1d8fd5ec42b1397b6704a73ffde2c",
+    "url": "/fonts/quicksand-v7-latin-500.54a1d8fd.woff"
+  },
+  {
+    "revision": "dce202681ee1fcdbdaf352303a4f383e",
+    "url": "/fonts/quicksand-v7-latin-500.dce20268.woff2"
+  },
+  {
+    "revision": "365396fdb9907773718c78332af9d536",
+    "url": "/fonts/quicksand-v7-latin-500.eot"
+  },
+  {
+    "revision": "f2610f3d011265ecf6de65d7c39d0245",
+    "url": "/fonts/quicksand-v7-latin-500.svg"
+  },
+  {
+    "revision": "53b5698c04e815736c8d5f91984b8945",
+    "url": "/fonts/quicksand-v7-latin-500.ttf"
+  },
+  {
+    "revision": "54a1d8fd5ec42b1397b6704a73ffde2c",
+    "url": "/fonts/quicksand-v7-latin-500.woff"
+  },
+  {
+    "revision": "dce202681ee1fcdbdaf352303a4f383e",
+    "url": "/fonts/quicksand-v7-latin-500.woff2"
+  },
+  {
+    "revision": "3c60bbddd10c7a2adae4555634ae5dd1",
+    "url": "/fonts/quicksand-v7-latin-700.3c60bbdd.eot"
+  },
+  {
+    "revision": "5cd19d36644dd28273fee690829842fd",
+    "url": "/fonts/quicksand-v7-latin-700.5cd19d36.woff"
+  },
+  {
+    "revision": "7a933eed108e0df9c758e36717c7e0f1",
+    "url": "/fonts/quicksand-v7-latin-700.7a933eed.ttf"
+  },
+  {
+    "revision": "8ec608d5ec889469c1ce8b70e06bdec2",
+    "url": "/fonts/quicksand-v7-latin-700.8ec608d5.woff2"
+  },
+  {
+    "revision": "3c60bbddd10c7a2adae4555634ae5dd1",
+    "url": "/fonts/quicksand-v7-latin-700.eot"
+  },
+  {
+    "revision": "f181240e901fc2217209430019eb6beb",
+    "url": "/fonts/quicksand-v7-latin-700.svg"
+  },
+  {
+    "revision": "7a933eed108e0df9c758e36717c7e0f1",
+    "url": "/fonts/quicksand-v7-latin-700.ttf"
+  },
+  {
+    "revision": "5cd19d36644dd28273fee690829842fd",
+    "url": "/fonts/quicksand-v7-latin-700.woff"
+  },
+  {
+    "revision": "8ec608d5ec889469c1ce8b70e06bdec2",
+    "url": "/fonts/quicksand-v7-latin-700.woff2"
+  },
+  {
+    "revision": "3f178e7b1a044e97e6ddf420f30e881b",
+    "url": "/fonts/quicksand-v7-latin-regular.3f178e7b.woff2"
+  },
+  {
+    "revision": "a1afbfae3b8b94644d165876e9f249cb",
+    "url": "/fonts/quicksand-v7-latin-regular.a1afbfae.eot"
+  },
+  {
+    "revision": "c52f5f2137c2d1fc2ae262930004f5b2",
+    "url": "/fonts/quicksand-v7-latin-regular.c52f5f21.woff"
+  },
+  {
+    "revision": "e07a6ac619d0259000f541118bbef780",
+    "url": "/fonts/quicksand-v7-latin-regular.e07a6ac6.ttf"
+  },
+  {
+    "revision": "a1afbfae3b8b94644d165876e9f249cb",
+    "url": "/fonts/quicksand-v7-latin-regular.eot"
+  },
+  {
+    "revision": "44e670efc467ba6a1e123ca2d622a302",
+    "url": "/fonts/quicksand-v7-latin-regular.svg"
+  },
+  {
+    "revision": "e07a6ac619d0259000f541118bbef780",
+    "url": "/fonts/quicksand-v7-latin-regular.ttf"
+  },
+  {
+    "revision": "c52f5f2137c2d1fc2ae262930004f5b2",
+    "url": "/fonts/quicksand-v7-latin-regular.woff"
+  },
+  {
+    "revision": "3f178e7b1a044e97e6ddf420f30e881b",
+    "url": "/fonts/quicksand-v7-latin-regular.woff2"
+  },
+  {
+    "revision": "419392ffddf623132aafc9f26fe9bc7c",
+    "url": "/images/cool.svg"
+  },
+  {
+    "revision": "4c86aa5e377ca760b4b8100f39aff467",
+    "url": "/images/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "9de722a94d3a23c14168e27885ea80ad",
+    "url": "/images/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "d9dbd2f276b289cdeb06bbdd5a06bba4",
+    "url": "/images/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "061b35aad5675278c0e1d00f6878af11",
+    "url": "/images/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "cec609d613546bccf9a40fda7c873fab",
+    "url": "/images/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "94a5a1d0ff31b92e9b01e48069339c83",
+    "url": "/images/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "5b1325bdaa0d8c57d89b4c38974bbfd9",
+    "url": "/images/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "cec609d613546bccf9a40fda7c873fab",
+    "url": "/images/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "1b09c1cfae8d8a0a90eca31d9d0b31cc",
+    "url": "/images/icons/badge-monochrome.png"
+  },
+  {
+    "revision": "cf3e24ee771ae4a0c157ad855499881a",
+    "url": "/images/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "7abdc7b12945af55dac37154360db471",
+    "url": "/images/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "9a894bb9068a370179f7fc7f3e88d716",
+    "url": "/images/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "bc47be20dcd6d99b47daf713b5dfbfd0",
+    "url": "/images/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "f22d501a35a87d9f21701cb031f6ea17",
+    "url": "/images/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "2dfadb25bc3ca25f2015ff4f9512a1f2",
+    "url": "/images/llama-nightscape.png"
+  },
+  {
+    "revision": "7e2fe5e5a1ccf25c9edcd3595c7d5e43",
+    "url": "/images/llama-nightscape.svg"
+  },
+  {
+    "revision": "aac5f36d5a57b086298906ff17713bb9",
+    "url": "/images/llama.svg"
+  },
+  {
+    "revision": "c379b734ce19b33ad5e393d638c2790d",
+    "url": "/images/logo-full-pride.svg"
+  },
+  {
+    "revision": "03e31425b8096b1be00bab17f2a950f6",
+    "url": "/images/logo-full-white.svg"
+  },
+  {
+    "revision": "91c03384369659c8adcc37f1dfa413e5",
+    "url": "/images/logo-full.svg"
+  },
+  {
+    "revision": "38fe46e693d65fd3674a688dde803cd5",
+    "url": "/images/logo.svg"
+  },
+  {
+    "revision": "2e16c6f794a933d469e29894273d91f6",
+    "url": "/images/migration/todoist.png"
+  },
+  {
+    "revision": "14fb5cbce5408946e4a46f9fb43a4f5c",
+    "url": "/images/migration/wunderlist.png"
+  },
+  {
+    "revision": "2dfadb25bc3ca25f2015ff4f9512a1f2",
+    "url": "/img/llama-nightscape.2dfadb25.png"
+  },
+  {
+    "revision": "aac5f36d5a57b086298906ff17713bb9",
+    "url": "/img/llama.aac5f36d.svg"
+  },
+  {
+    "revision": "2e00b2635b51ba336b4b67a5d0bc03c7",
+    "url": "/img/open-sans-v15-latin-700.2e00b263.svg"
+  },
+  {
+    "revision": "f445cf25b8e059587318c7ee9014154b",
+    "url": "/img/open-sans-v15-latin-700italic.f445cf25.svg"
+  },
+  {
+    "revision": "fd2ba7503f70aafcdcc45211a481684d",
+    "url": "/img/open-sans-v15-latin-italic.fd2ba750.svg"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/img/open-sans-v15-latin-regular.7aab4c13.svg"
+  },
+  {
+    "revision": "4e010b929d0b71314a368114b9c66a83",
+    "url": "/img/quicksand-v7-latin-300.4e010b92.svg"
+  },
+  {
+    "revision": "f2610f3d011265ecf6de65d7c39d0245",
+    "url": "/img/quicksand-v7-latin-500.f2610f3d.svg"
+  },
+  {
+    "revision": "f181240e901fc2217209430019eb6beb",
+    "url": "/img/quicksand-v7-latin-700.f181240e.svg"
+  },
+  {
+    "revision": "44e670efc467ba6a1e123ca2d622a302",
+    "url": "/img/quicksand-v7-latin-regular.44e670ef.svg"
+  },
+  {
+    "revision": "2698b88bcfc0b34dbc5de52167202393",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a67931e5df0a8d7c94e3",
+    "url": "/js/app.c5e340e6.js"
+  },
+  {
+    "revision": "758459fa343dbfd43db7",
+    "url": "/js/chunk-vendors.39aed666.js"
+  },
+  {
+    "revision": "f0f69663654ba985693f",
+    "url": "/js/editor.dc1d073e.js"
+  },
+  {
+    "revision": "cffb11925256e011f5a9",
+    "url": "/js/multiselect.b5f49f60.js"
+  },
+  {
+    "revision": "adcaee5f63f9ebe304ca",
+    "url": "/js/settings.8e2d2394.js"
+  },
+  {
+    "revision": "5d12c2828e34801d2a41",
+    "url": "/js/user-settings.0a08dca5.js"
+  },
+  {
+    "revision": "04f626f96a4bd9988026e6b5ab1f30d8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ sw.js
@@ -0,0 +1,115 @@
+importScripts("/precache-manifest.3ec5b38fde30f551e285d03b4b2604a7.js", "/workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/workbox-v4.3.1"});
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+
+// Cache assets
+workbox.routing.registerRoute(
+    // This regexp matches all files in precache-manifest
+    new RegExp('.+\\.(css|json|js|eot|svg|ttf|woff|woff2|png|html|txt)$'),
+    new workbox.strategies.StaleWhileRevalidate()
+);
+
+// Always send api reqeusts through the network
+workbox.routing.registerRoute(
+	new RegExp('api\\/v1\\/.*$'),
+	new workbox.strategies.NetworkOnly()
+);
+
+// This code listens for the user's confirmation to update the app.
+self.addEventListener('message', (e) => {
+	if (!e.data) {
+		return;
+	}
+
+	switch (e.data) {
+		case 'skipWaiting':
+			self.skipWaiting();
+			break;
+		default:
+			// NOOP
+			break;
+	}
+});
+
+const getBearerToken = async () => {
+	// we can't get a client that sent the current request, therefore we need
+	// to ask any controlled page for auth token
+	const allClients = await self.clients.matchAll();
+	const client = allClients.filter(client => client.type === 'window')[0];
+
+	// if there is no page in scope, we can't get any token
+	// and we indicate it with null value
+	if(!client) {
+		return null;
+	}
+
+	// to communicate with a page we will use MessageChannels
+	// they expose pipe-like interface, where a receiver of
+	// a message uses one end of a port for messaging and
+	// we use the other end for listening
+	const channel = new MessageChannel();
+
+	client.postMessage({
+		'action': 'getBearerToken'
+	}, [channel.port1]);
+
+	// ports support only onmessage callback which
+	// is cumbersome to use, so we wrap it with Promise
+	return new Promise((resolve, reject) => {
+		channel.port2.onmessage = event => {
+			if (event.data.error) {
+				console.error('Port error', event.error);
+				reject(event.data.error);
+			}
+
+			resolve(event.data.authToken);
+		}
+	});
+}
+
+// Notification action
+self.addEventListener('notificationclick', function(event) {
+	const taskId = event.notification.data.taskId
+	event.notification.close()
+
+	switch (event.action) {
+		case 'mark-as-done':
+			// FIXME: Ugly as hell, but no other way of doing this, since we can't use modules
+			// in service workersfor now.
+			fetch('/config.json')
+				.then(r => r.json())
+				.then(config => {
+
+					getBearerToken()
+						.then(token => {
+							fetch(`${config.VIKUNJA_API_BASE_URL}tasks/${taskId}`, {
+								method: 'post',
+								headers: {
+									'Accept': 'application/json',
+									'Content-Type': 'application/json',
+									'Authorization': `Bearer ${token}`,
+								},
+								body: JSON.stringify({id: taskId, done: true})
+							})
+							.then(r => r.json())
+							.then(r => {
+								console.debug('Task marked as done from notification', r)
+							})
+							.catch(e => {
+								console.debug('Error marking task as done from notification', e)
+							})
+						})
+				})
+			break
+		case 'show-task':
+			clients.openWindow(`/tasks/${taskId}`)
+			break
+	}
+})
+
+workbox.core.clientsClaim();
+// The precaching code provided by Workbox.
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
